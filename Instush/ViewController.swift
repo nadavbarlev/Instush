d8a07d97ec7c5f4acf07f5d0980bf31839a7c39e
Nadav Bar Lev <nadav.bar.lev@gmail.com>
2018-11-04 13:56:35 +0200

    Initial Commit

---
+++ Instush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instush
+//
+//  Created by Nadav Bar Lev on 04/11/2018.
+//  Copyright © 2018 Nadav Bar Lev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
